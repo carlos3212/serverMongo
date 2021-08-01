d9fb7a2d8464377ca00fe3c58bbfa9d6e8904c8e
carlos <carlosAndrade@users.noreply.github.com>
2021-08-01 17:53:01 -0500

    login1

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -75,7 +75,6 @@ app.put('/usuario/:id', function(req, res) {
 
 app.post('/usuario', function(req, res) {
     let body = req.body;
-    
 
     let usuario = new Usuario({
         nombre: body.nombre,
@@ -84,46 +83,7 @@ app.post('/usuario', function(req, res) {
         role: body.role
     });
     
-app.post('/login', function (req, res) {
-        let body = req.body;
-        Usuario.findOne({ email: body.email }, (erro, usuarioDB)=>{
-            if (err) {
-            return res.status(500).json({
-                ok: false,
-                err: erro
-            })
-        }
-    // Verifica que exista un usuario con el mail escrita por el usuario.
-        if (!usuarioDB) {
-            return res.status(400).json({
-            ok: false,
-            err: {
-                message: "Usuario o contraseña incorrectos"
-            }
-            })
-        }
-    // Valida que la contraseña escrita por el usuario, sea la almacenada en la db
-        if (! bcrypt.compareSync(body.password, usuarioDB.password)){
-            return res.status(400).json({
-                ok: false,
-                err: {
-                message: "Usuario o contraseña incorrectos"
-                }
-            });
-        }
-    // Genera el token de autenticación
-        let token = jwt.sign({
-                usuario: usuarioDB,
-            }, process.env.SEED_AUTENTICACION, {
-            expiresIn: process.env.CADUCIDAD_TOKEN
-        })
-        res.json({
-            ok: true,
-            usuario: usuarioDB,
-            token,
-        })
-    })
-    })
+
 
     usuario.save((err, usuarioDB) => {
         if (err) {
@@ -140,6 +100,47 @@ app.post('/login', function (req, res) {
 
     });
 
+});
+
+app.post('/login', function (req, res) {
+    let body = req.body;
+    Usuario.findOne({ email: body.email }, (erro, usuarioDB)=>{
+        if (erro) {
+          return res.status(500).json({
+             ok: false,
+             err: erro
+          })
+       }
+   // Verifica que exista un usuario con el mail escrita por el usuario.
+      if (!usuarioDB) {
+         return res.status(400).json({
+           ok: false,
+           err: {
+               message: "Usuario o contraseña incorrectos"
+           }
+        })
+      }
+   // Valida que la contraseña escrita por el usuario, sea la almacenada en la db
+      if (! bcrypt.compareSync(body.password, usuarioDB.password)){
+         return res.status(400).json({
+            ok: false,
+            err: {
+              message: "Usuario o contraseña incorrectos"
+            }
+         });
+      }
+   // Genera el token de autenticación
+       let token = jwt.sign({
+              usuario: usuarioDB,
+           }, process.env.SEED_AUTENTICACION, {
+           expiresIn: process.env.CADUCIDAD_TOKEN
+       })
+       res.json({
+           ok: true,
+           usuario: usuarioDB,
+           token,
+       })
+   })
 });
 
 
